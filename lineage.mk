60b98620e4aac77db70ad52435a4d5dbf3d7c282
SpiritFive <nicola.scotti99@gmail.com>
2018-12-20 00:49:30 +0100

    Added z500m makefile and z500m vendor make file.

--- lineage.mk
+++ lineage.mk
@@ -1,17 +1,15 @@
-#TODO: find it
-$(call inherit-product, device/gpd/xds/xds.mk)
+$(call inherit-product, device/asus/z500m/z500m.mk)
 
-#TODO
 # Common CM stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT="asus/WW_P027/P027:7.0/NRD90M/14.0210.1806.33-20180621:user/release-keys" PRIVATE_BUILD_DESC="WW_ZenPad-user 7.0 NRD90M 14.0210.1806.33-20180621 release-keys"
 
-PRODUCT_NAME := lineage_xds
-PRODUCT_DEVICE := z500m
-PRODUCT_BRAND := Asus
-PRODUCT_MANUFACTURER := Asus
-PRODUCT_MODEL := z500m
+PRODUCT_NAME := lineage_z500m
+PRODUCT_DEVICE := P027
+PRODUCT_BRAND := asus
+PRODUCT_MANUFACTURER := asus
+PRODUCT_MODEL := P027
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-PRODUCT_DEVICE="z500m"
+PRODUCT_DEVICE="P027"
